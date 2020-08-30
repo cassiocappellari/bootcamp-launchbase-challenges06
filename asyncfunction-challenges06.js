4f8e831224d1251db8c6635c407d809e208e4a5e
Cássio <cassiocappellari@gmail.com>
2020-08-30 16:58:55 -0300

    async/await challenge done

--- asyncfunction-challenges06.js
+++ asyncfunction-challenges06.js
@@ -1,6 +1,6 @@
 // callback
 
-/* function printDouble(number, callback) {
+/*function printDouble(number, callback) {
     setTimeout(function() {
         console.log(number * 2)
         callback()
@@ -21,11 +21,11 @@ function printAll() {
     })
 }
 
-printAll() */
+printAll()*/
 
 // promises
 
-let printDoubleNumber = number => new Promise((resolve, reject) => {
+/*let printDoubleNumber = number => new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve(number * 2)
     },
@@ -33,18 +33,49 @@ let printDoubleNumber = number => new Promise((resolve, reject) => {
     )
 })
 
-printDoubleNumber(5).then((results) => {
+printDoubleNumber(5)
+.then((results) => {
     console.log(results)
-    printDoubleNumber(10).then((results) => {
-        console.log(results)
-        printDoubleNumber(22).then((results) => {
-            console.log(results)
-            printDoubleNumber(1).then((results) => {
-                console.log(results)
-                printDoubleNumber(89).then((results) => {
-                    console.log(results)
-                })
-            })
-        })
-    })
+printDoubleNumber(10)
+.then((results) => {
+    console.log(results)
+printDoubleNumber(22)
+.then((results) => {
+    console.log(results)
+printDoubleNumber(1)
+.then((results) => {
+    console.log(results)
+printDoubleNumber(89)
+.then((results) => {
+    console.log(results)
 })
+})
+})
+})
+})*/
+
+// async/await
+
+async function printDoubleNumber(number) {
+    return await new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(number * 2)
+        }, Math.floor(Math.random() * 100) + 1)
+    })
+}
+
+const printAll = async () => {
+    let results
+    results = await printDoubleNumber(5)
+    console.log(results)
+    results = await printDoubleNumber(10)
+    console.log(results)
+    results = await printDoubleNumber(22)
+    console.log(results)
+    results = await printDoubleNumber(1)
+    console.log(results)
+    results = await printDoubleNumber(89)
+    console.log(results)
+}
+
+printAll()
